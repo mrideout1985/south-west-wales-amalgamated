704c3ccd0e345fbb8a55e57606d700f304810b9b
Matthew <mrideout1985@gmail.com>
2021-10-19 19:37:47 +0100

    removed header as child and put inside layout comp

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import Layout from "../components/layout/layout";
-import Header from "../components/header/header";
-import "../styles/main.scss";
-import "../styles/globals.scss";
+import "../main.scss"
+
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
-			<Header />
 			<Component {...pageProps} />
 		</Layout>
 	);
