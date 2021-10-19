f81fa691eea01553f6d8b35f71b8b46868a23cc6
Matthew <mrideout1985@gmail.com>
2021-10-19 19:35:40 +0100

    added nav component to header

--- components/header/header.js
+++ components/header/header.js
@@ -1,20 +1,22 @@
 import React from "react";
-import Image from "next/image";
-import dragon from "../../assets/images/dragon.svg";
-
+import Dragon from "../icons/Dragon"
+import CwuLogo from "../icons/CwuLogo"
 import styles from "./header.module.scss";
+import Nav from "../navbar/nav";
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
 				<h1>South West Wales AMAL</h1>
-				<span>
-					<Image src={dragon} alt="dragon" width={100} height={100} />
-				</span>
+				<div className={styles.logos}>
+					<CwuLogo size={100} />
+					<Dragon size={100} />
+				</div>
 			</div>
+			<Nav />
 		</header>
 	);
 };
 
-export { Header };
+export default Header
