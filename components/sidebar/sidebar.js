b9dd2ed859ed3aa442454b3f96f676362d30e116
Matthew <mrideout1985@gmail.com>
2021-10-25 22:33:45 +0100

    sidebar

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -3,10 +3,12 @@ import Link from "next/link";
 import Image from "next/image"
 import styles from "./sidebar.module.scss";
 import { useClickAway } from "react-use";
+import ThreeBars from "../icons/ThreeBars"
+import CwuLogo from "../icons/CwuLogo"
 
 const Sidebar = () => {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 	const sidebarRef = useRef();
 
 	useClickAway(sidebarRef, () => {
@@ -18,43 +20,43 @@ const Sidebar = () => {
 		setIsSidebarOpen(!isSidebarOpen);
 	};
 
-	useEffect(() => {
-		let focusableElements = document.querySelectorAll("a[href]");
-		let firstTabStop = focusableElements[0];
-		let lastTabStop = focusableElements[focusableElements.length - 1];
+	// useEffect(() => {
+	// 	let focusableElements = document.querySelectorAll("a[href]");
+	// 	let firstTabStop = focusableElements[0];
+	// 	let lastTabStop = focusableElements[focusableElements.length - 1];
 
-		console.log(firstTabStop);
+	// 	console.log(firstTabStop);
 
-		if (isSidebarOpen) {
-			focusableElements = Array.prototype.slice.call([focusableElements]);
-			firstTabStop.focus();
+	// 	if (isSidebarOpen) {
+	// 		focusableElements = Array.prototype.slice.call([focusableElements]);
+	// 		firstTabStop.focus();
 
-			const handleTabKey = (event) => {
-				if (event.key === "Tab") {
-					if (event.shiftKey) {
-						if (document.activeElement === firstTabStop) {
-							event.preventDefault();
-							lastTabStop.focus();
-						}
-					} else {
-						if (document.activeElement === lastTabStop) {
-							event.preventDefault();
-							firstTabStop.focus();
-						}
-					}
-				}
-			};
+	// 		const handleTabKey = (event) => {
+	// 			if (event.key === "Tab") {
+	// 				if (event.shiftKey) {
+	// 					if (document.activeElement === firstTabStop) {
+	// 						event.preventDefault();
+	// 						lastTabStop.focus();
+	// 					}
+	// 				} else {
+	// 					if (document.activeElement === lastTabStop) {
+	// 						event.preventDefault();
+	// 						firstTabStop.focus();
+	// 					}
+	// 				}
+	// 			}
+	// 		};
 
-			const handleEscapeKey = (event) => {
-				if (event.key === "Escape") {
-					setIsSidebarOpen(!isSidebarOpen);
-				}
-			};
+	// 		const handleEscapeKey = (event) => {
+	// 			if (event.key === "Escape") {
+	// 				setIsSidebarOpen(!isSidebarOpen);
+	// 			}
+	// 		};
 
-			document.addEventListener("keydown", handleTabKey);
-			document.addEventListener("keydown", handleEscapeKey);
-		}
-	}, [isSidebarOpen]);
+	// 		document.addEventListener("keydown", handleTabKey);
+	// 		document.addEventListener("keydown", handleEscapeKey);
+	// 	}
+	// }, [isSidebarOpen]);
 
 	return (
 		<>
@@ -66,45 +68,38 @@ const Sidebar = () => {
 				ref={sidebarRef}
 				aria-label="sidebar"
 			>
-				<div className={styles["heading"]}>
-					<h1>
-						<Link href="/">SWWAMAL</Link>
-					</h1>
+				<div className={styles.logo}>
+					<CwuLogo size={100} />
 				</div>
-				<div className={styles["nav-container"]}>
-					<nav className={styles["nav"]}>
-						<ul>
-							<li>
-								<Link href="/home">Home</Link>
-							</li>
-							<li>
-								<Link href="/about">About</Link>
-							</li>
-							<li>
-								<Link href="/news">News</Link>
-							</li>
-							<li>
-								<Link href="/campaigns">Campaigns</Link>
-							</li>
-							<li>
-								<Link href="/agreements">Agreements</Link>
-							</li>
-							<li>
-								<Link href="/blog">Blog</Link>
-							</li>
-							<li>
-								<Link href="/contact">Contact</Link>
-							</li>
-						</ul>
-					</nav>
-				</div>
+				<ul>
+					<li>
+						<Link href="/">Home</Link>
+					</li>
+					<li>
+						<Link href="/about">About</Link>
+					</li>
+					<li>
+						<Link href="/news">News</Link>
+					</li>
+					<li>
+						<Link href="/campaigns">Campaigns</Link>
+					</li>
+					<li>
+						<Link href="/agreements">Agreements</Link>
+					</li>
+					<li>
+						<Link href="/blog">Blog</Link>
+					</li>
+					<li>
+						<Link href="/contact">Contact</Link>
+					</li>
+				</ul>
 			</div>
 			<button
 				className={styles["sidebar-btn"]}
 				onClick={(e) => handleToggle(e)}
-				aria-expanded={isSidebarOpen}
-			>
-				<Image src={"/public/bars.svg"} height={10} width={10} />
+				aria-expanded={isSidebarOpen}>
+				<ThreeBars />
 			</button>
 		</>
 	);
