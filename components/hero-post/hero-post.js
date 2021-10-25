e30162329dde5c0107a10413130f0802628f770c
Matthew <mrideout1985@gmail.com>
2021-10-25 22:34:42 +0100

    hero-post component

---
+++ components/hero-post/hero-post.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Avatar from '../avatar/avatar'
+import CoverImage from '../cover-image/cover-image'
+import { imageBuilder } from "../../lib/sanity"
+import Link from "next/link"
+import Date from '../date/date'
+import styles from "./hero-post.module.scss"
+
+const HeroPost = ({ title, coverImage, date, excerpt, author, slug }) => {
+	const image = (
+		<img
+			width={1240}
+			height={540}
+			alt={`Cover Image for ${title}`}
+			src={imageBuilder(coverImage).width(1240).height(540).url()}
+		/>
+	)
+	return (
+		<section className={styles.container}>
+			<div className={styles.coverimage}>
+				<CoverImage slug={slug} imageObject={coverImage} title={title} url={coverImage} />
+			</div>
+			<div className={styles.postinfo}>
+				<div className={styles.title_date}>
+					<h3 className={styles.h3}>
+						<Link as={`/posts/${slug}`} href="/posts/[slug]">
+							<a>{title}</a>
+						</Link>
+					</h3>
+					<div className={styles.date}>
+						<Date dateString={date} />
+					</div>
+				</div>
+				<div className={styles.excerpt_author}>
+					<p className={styles.text}>{excerpt}</p>
+					<Avatar name={author?.name} picture={author?.picture} />
+				</div>
+			</div>
+		</section >
+	)
+}
+
+export default HeroPost
