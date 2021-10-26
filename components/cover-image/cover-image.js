783aee6a56b154e5d7c8d91899f5fb2995f46710
Matthew <mrideout1985@gmail.com>
2021-10-26 15:26:31 +0100

    changed image size

--- components/cover-image/cover-image.js
+++ components/cover-image/cover-image.js
@@ -1,23 +1,25 @@
 import cn from 'classnames'
 import Link from 'next/link'
 import { imageBuilder } from "../../lib/sanity"
+import { useRouter } from 'next/router'
 import styles from "./cover-image.module.scss"
 
 export default function CoverImage({ title, url, imageObject, slug }) {
+
+	const path = useRouter()
+
 	const image = (
 		<img
-			width={1240}
-			height={540}
 			alt={`Cover Image for ${title}`}
 			className={styles.image}
-			src={imageBuilder(imageObject).width(1240).height(540).url()}
+			src={imageBuilder(imageObject).width(1400).height(540).url()}
 		/>
 	)
 
 	return (
 		<div className={styles.container}>
 			{slug ? (
-				<Link as={`/posts/${slug}`} href="/posts/[slug]">
+				<Link href={path.asPath === `/posts/` ? `/posts/${slug}` : `/campaigns/${slug}`}>
 					<a aria-label={title}>{image}</a>
 				</Link>
 			) : (
