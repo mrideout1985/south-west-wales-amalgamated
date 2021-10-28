a6232ea114fb3cf9e04efb33ca91ed4bf7fc14cf
Matthew <mrideout1985@gmail.com>
2021-10-28 07:36:02 +0100

    ?bug

--- pages/campaigns/[slug].js
+++ pages/campaigns/[slug].js
@@ -6,27 +6,31 @@ import styles from "../../styles/Post.module.scss"
 import ErrorPage from 'next/error'
 import { getAllCampaignDataWithSlug, getCampaignsAndMoreCampaigns } from '../../lib/api'
 
-const Campaigns = ({ post, morePosts, preview }) => {
+// const Campaigns = ({ post, morePosts, preview }) => {
+
+const Campaigns = () => {
+
+
 
 	return (
 		<div>
-			<article className={styles.container}>
+			{/* <article className={styles.container}>
 				<div className={styles.title}>
 					<h2>{post.title !== undefined ? post.title : "Title"}</h2>
 				</div>
 
 				<div className={styles.post}>
-					{/* {post.body === undefined ? null : <SanityBlockContent
+					{post.body === undefined ? null : <SanityBlockContent
 						dataset="production"
 						projectId="8bvty42v"
 						blocks={post.body}
-					/>} */}
+					/>}
 				</div>
 				<div className={styles.avatar}>
 					<Avatar name={post.author?.name} picture={post.author?.picture} />
 				</div>
-			</article>
-			)
+			</article> */}
+			123
 
 		</div>
 	)
@@ -34,27 +38,27 @@ const Campaigns = ({ post, morePosts, preview }) => {
 
 export default Campaigns
 
-export async function getStaticProps({ params, preview = false }) {
-	const data = await getCampaignsAndMoreCampaigns(params.slug, preview)
-	return {
-		props: {
-			preview,
-			post: data?.campaigns || null,
-			morePosts: data?.morePosts || null,
-		},
-		revalidate: 1
-	}
-}
+// export async function getStaticProps({ params, preview = false }) {
+// 	const data = await getCampaignsAndMoreCampaigns(params.slug, preview)
+// 	return {
+// 		props: {
+// 			preview,
+// 			post: data?.campaigns || null,
+// 			morePosts: data?.morePosts || null,
+// 		},
+// 		revalidate: 1
+// 	}
+// }
 
-export async function getStaticPaths() {
-	const allPosts = await getAllCampaignDataWithSlug()
-	return {
-		paths:
-			allPosts?.map((post) => ({
-				params: {
-					slug: post.slug,
-				},
-			})) || [],
-		fallback: true,
-	}
-}
+// export async function getStaticPaths() {
+// 	const allPosts = await getAllCampaignDataWithSlug()
+// 	return {
+// 		paths:
+// 			allPosts?.map((post) => ({
+// 				params: {
+// 					slug: post.slug,
+// 				},
+// 			})) || [],
+// 		fallback: true,
+// 	}
+// }
