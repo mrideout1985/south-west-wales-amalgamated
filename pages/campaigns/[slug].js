8dfd3af6d2e2aaeae840fd81c87edb9d4ec29159
Matthew <mrideout1985@gmail.com>
2021-10-28 06:46:42 +0100

    try #999

--- pages/campaigns/[slug].js
+++ pages/campaigns/[slug].js
@@ -1,61 +1,61 @@
-// import React from 'react'
-// import { useRouter } from 'next/router'
-// import SanityBlockContent from "@sanity/block-content-to-react";
-// import Avatar from "../../components/avatar/avatar"
-// import styles from "../../styles/Post.module.scss"
-// import ErrorPage from 'next/error'
-// import { getAllCampaignDataWithSlug, getCampaignsAndMoreCampaigns } from '../../lib/api'
+import React from 'react'
+import { useRouter } from 'next/router'
+import SanityBlockContent from "@sanity/block-content-to-react";
+import Avatar from "../../components/avatar/avatar"
+import styles from "../../styles/Post.module.scss"
+import ErrorPage from 'next/error'
+import { getAllCampaignDataWithSlug, getCampaignsAndMoreCampaigns } from '../../lib/api'
 
-// const Campaigns = ({ post }) => {
+const Campaigns = ({ post }) => {
 
-// 	return (
-// 		<div>
-// 			<article className={styles.container}>
-// 				<div className={styles.title}>
-// 					<h2>{post.title}</h2>
-// 				</div>
+	return (
+		<div>
+			<article className={styles.container}>
+				<div className={styles.title}>
+					<h2>{post.title}</h2>
+				</div>
 
-// 				<div className={styles.post}>
-// 					{post.body === undefined ? null : <SanityBlockContent
-// 						dataset="production"
-// 						projectId="8bvty42v"
-// 						blocks={post.body}
-// 					/>}
-// 				</div>
-// 				<div className={styles.avatar}>
-// 					<Avatar name={post.author?.name} picture={post.author?.picture} />
-// 				</div>
-// 			</article>
-// 			)
+				<div className={styles.post}>
+					{post.body === undefined ? null : <SanityBlockContent
+						dataset="production"
+						projectId="8bvty42v"
+						blocks={post.body}
+					/>}
+				</div>
+				<div className={styles.avatar}>
+					<Avatar name={post.author?.name} picture={post.author?.picture} />
+				</div>
+			</article>
+			)
 
-// 		</div>
-// 	)
-// }
+		</div>
+	)
+}
 
-// export default Campaigns
+export default Campaigns
 
-// export async function getStaticProps({ params, preview = false }) {
-// 	const data = await getCampaignsAndMoreCampaigns(params.slug, preview)
-// 	return {
-// 		props: {
-// 			preview,
-// 			post: data?.campaigns || null,
-// 			morePosts: data?.morePosts || null,
-// 		},
-// 		revalidate: 1
-// 	}
-// }
+export async function getStaticProps({ params, preview = false }) {
+	const data = await getCampaignsAndMoreCampaigns(params.slug, preview)
+	return {
+		props: {
+			preview,
+			post: data?.campaigns || null,
+			morePosts: data?.morePosts || null,
+		},
+		revalidate: 1
+	}
+}
 
-// export async function getStaticPaths() {
-// 	const allPosts = await getAllCampaignDataWithSlug()
-// 	console.log("allPosts", allPosts)
-// 	return {
-// 		paths:
-// 			allPosts?.map((post) => ({
-// 				params: {
-// 					slug: post.slug,
-// 				},
-// 			})) || [],
-// 		fallback: true,
-// 	}
-// }
+export async function getStaticPaths() {
+	const allPosts = await getAllCampaignDataWithSlug()
+	console.log("allPosts", allPosts)
+	return {
+		paths:
+			allPosts?.map((post) => ({
+				params: {
+					slug: post.slug,
+				},
+			})) || [],
+		fallback: true,
+	}
+}
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,65 +1,65 @@
-// import { useRouter } from 'next/router'
-// import ErrorPage from 'next/error'
-// import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
-// import SanityBlockContent from "@sanity/block-content-to-react";
-// import Avatar from "../../components/avatar/avatar"
-// import styles from "../../styles/Post.module.scss"
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
+import SanityBlockContent from "@sanity/block-content-to-react";
+import Avatar from "../../components/avatar/avatar"
+import styles from "../../styles/Post.module.scss"
 
-// export default function Post({ post, morePosts, preview }) {
-//   const router = useRouter()
-//   if (!router.isFallback && !post?.slug) {
-//     return <ErrorPage statusCode={404} />
-//   }
-//   console.log(morePosts);
-//   console.log(preview)
-//   return (
-//     <div>
-//       {
-//         router.isFallback ? (
-//           <div>Loading…</div>
-//         ) : (
-//           <article className={styles.container}>
-//             <div className={styles.title}>
-//               <h2>{post.title}</h2>
-//             </div>
-//             <div className={styles.post}>
-//               {post.body === undefined ? null : <SanityBlockContent
-//                 dataset="production"
-//                 projectId="8bvty42v"
-//                 blocks={post.body}
-//               />}
-//             </div>
-//             <div className={styles.avatar}>
-//               <Avatar name={post.author.name} picture={post.author.picture} />
-//             </div>
-//           </article>
-//         )
-//       }
-//     </div>
-//   )
-// }
+export default function Post({ post, morePosts, preview }) {
+  const router = useRouter()
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  console.log(morePosts);
+  console.log(preview)
+  return (
+    <div>
+      {
+        router.isFallback ? (
+          <div>Loading…</div>
+        ) : (
+          <article className={styles.container}>
+            <div className={styles.title}>
+              <h2>{post.title}</h2>
+            </div>
+            <div className={styles.post}>
+              {post.body === undefined ? null : <SanityBlockContent
+                dataset="production"
+                projectId="8bvty42v"
+                blocks={post.body}
+              />}
+            </div>
+            <div className={styles.avatar}>
+              <Avatar name={post.author.name} picture={post.author.picture} />
+            </div>
+          </article>
+        )
+      }
+    </div>
+  )
+}
 
-// export async function getStaticProps({ params, preview = false }) {
-//   const data = await getPostAndMorePosts(params.slug, preview)
-//   return {
-//     props: {
-//       preview,
-//       post: data?.post || null,
-//       morePosts: data?.morePosts || null,
-//     },
-//     revalidate: 1
-//   }
-// }
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getPostAndMorePosts(params.slug, preview)
+  return {
+    props: {
+      preview,
+      post: data?.post || null,
+      morePosts: data?.morePosts || null,
+    },
+    revalidate: 1
+  }
+}
 
-// export async function getStaticPaths() {
-//   const allPosts = await getAllPostsWithSlug()
-//   return {
-//     paths:
-//       allPosts?.map((post) => ({
-//         params: {
-//           slug: post.slug,
-//         },
-//       })) || [],
-//     fallback: true,
-//   }
-// }
+export async function getStaticPaths() {
+  const allPosts = await getAllPostsWithSlug()
+  return {
+    paths:
+      allPosts?.map((post) => ({
+        params: {
+          slug: post.slug,
+        },
+      })) || [],
+    fallback: true,
+  }
+}
