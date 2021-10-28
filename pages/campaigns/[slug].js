61df49e4697fa4572406f767d418305e705ef343
Matthew <mrideout1985@gmail.com>
2021-10-28 06:40:13 +0100

    taken out slugs to try and fix error

--- pages/campaigns/[slug].js
+++ pages/campaigns/[slug].js
@@ -6,38 +6,28 @@
 // import ErrorPage from 'next/error'
 // import { getAllCampaignDataWithSlug, getCampaignsAndMoreCampaigns } from '../../lib/api'
 
-// const Campaigns = ({ post, morePosts, preview }) => {
-// 	const router = useRouter()
-// 	if (!router.isFallback && !post?.slug) {
-// 		return <ErrorPage statusCode={404} />
-// 	}
-// 	console.log(morePosts);
-// 	console.log(preview)
+// const Campaigns = ({ post }) => {
+
 // 	return (
 // 		<div>
-// 			{
-// 				router.isFallback ? (
-// 					<div>Loading…</div>
-// 				) : (
+// 			<article className={styles.container}>
+// 				<div className={styles.title}>
+// 					<h2>{post.title}</h2>
+// 				</div>
 
-// 					<article className={styles.container}>
-// 						<div className={styles.title}>
-// 							<h2>{post.title}</h2>
-// 						</div>
+// 				<div className={styles.post}>
+// 					{post.body === undefined ? null : <SanityBlockContent
+// 						dataset="production"
+// 						projectId="8bvty42v"
+// 						blocks={post.body}
+// 					/>}
+// 				</div>
+// 				<div className={styles.avatar}>
+// 					<Avatar name={post.author?.name} picture={post.author?.picture} />
+// 				</div>
+// 			</article>
+// 			)
 
-// 						<div className={styles.post}>
-// 							{post.body === undefined ? null : <SanityBlockContent
-// 								dataset="production"
-// 								projectId="8bvty42v"
-// 								blocks={post.body}
-// 							/>}
-// 						</div>
-// 						<div className={styles.avatar}>
-// 							<Avatar name={post.author?.name} picture={post.author?.picture} />
-// 						</div>
-// 					</article>
-// 				)
-// 			}
 // 		</div>
 // 	)
 // }
