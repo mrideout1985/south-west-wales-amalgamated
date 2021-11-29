c12b535764eaf56ed92ac2ace8546ac26907871a
mrideout1985 <mrideout1985@gmail.com>
2021-11-29 20:33:44 +0000

    fixed link error

--- components/cover-image/cover-image.js
+++ components/cover-image/cover-image.js
@@ -1,30 +1,33 @@
-import cn from 'classnames'
-import Link from 'next/link'
-import { imageBuilder } from "../../lib/sanity"
-import { useRouter } from 'next/router'
-import styles from "./cover-image.module.scss"
+import cn from "classnames";
+import Link from "next/link";
+import { imageBuilder } from "../../lib/sanity";
+import { useRouter } from "next/router";
+import styles from "./cover-image.module.scss";
 
 export default function CoverImage({ title, url, imageObject, slug }) {
+  const path = useRouter();
 
-	const path = useRouter()
+  const image = (
+    <img
+      alt={`Cover Image for ${title}`}
+      className={styles.image}
+      src={imageBuilder(imageObject).width(1400).height(540).url()}
+    />
+  );
 
-	const image = (
-		<img
-			alt={`Cover Image for ${title}`}
-			className={styles.image}
-			src={imageBuilder(imageObject).width(1400).height(540).url()}
-		/>
-	)
-
-	return (
-		<div className={styles.container}>
-			{slug ? (
-				<Link href={path.asPath === `/blog` ? `/posts/${slug}` : `/campaigns/${slug}`}>
-					<a aria-label={title}>{image}</a>
-				</Link>
-			) : (
-				image
-			)}
-		</div>
-	)
+  return (
+    <div className={styles.container}>
+      {slug ? (
+        <Link
+          href={
+            path.asPath === `/news` ? `/posts/${slug}` : `/campaigns/${slug}`
+          }
+        >
+          <a aria-label={title}>{image}</a>
+        </Link>
+      ) : (
+        image
+      )}
+    </div>
+  );
 }
--- components/post-preview/post-preview.js
+++ components/post-preview/post-preview.js
@@ -1,33 +1,35 @@
+import Link from "next/link";
+import { imageBuilder } from "../../lib/sanity";
+import { useRouter } from "next/router";
+import CoverImage from "../cover-image/cover-image";
+import Date from "../date/date";
+import Avatar from "../avatar/avatar";
+import styles from "./post-preview.module.scss";
+export default function PostPreview({ title, coverImage, date, slug }) {
+  const path = useRouter();
 
-import Link from 'next/link'
-import { imageBuilder } from '../../lib/sanity'
-import { useRouter } from 'next/router'
-import CoverImage from '../cover-image/cover-image'
-import Date from '../date/date'
-import Avatar from '../avatar/avatar'
-import styles from "./post-preview.module.scss"
-export default function PostPreview({
-	title,
-	coverImage,
-	date,
-	slug,
-}) {
-
-	const path = useRouter()
-
-	return (
-		<div className={styles.container}>
-			<div className={styles.image}>
-				<CoverImage slug={slug} title={title} imageObject={coverImage} url={imageBuilder(coverImage).url()} />
-			</div>
-			<h3>
-				<Link href={path.asPath === `/blog` ? `/posts/${slug}` : `/campaigns/${slug}`}>
-					<a >{title}</a>
-				</Link>
-			</h3>
-			<div className={styles.date} >
-				<Date dateString={date} />
-			</div>
-		</div>
-	)
+  return (
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <CoverImage
+          slug={slug}
+          title={title}
+          imageObject={coverImage}
+          url={imageBuilder(coverImage).url()}
+        />
+      </div>
+      <h3>
+        <Link
+          href={
+            path.asPath === `/news` ? `/posts/${slug}` : `/campaigns/${slug}`
+          }
+        >
+          <a>{title}</a>
+        </Link>
+      </h3>
+      <div className={styles.date}>
+        <Date dateString={date} />
+      </div>
+    </div>
+  );
 }
